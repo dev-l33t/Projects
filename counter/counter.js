e421c4fb74fcff8185fd6b71750f7068048799f1
dev-l33t <desk.pratikjoshi@gmail.com>
2025-05-09 22:49:26 +0530

    Create counter.js

---
+++ counter/counter.js
@@ -0,0 +1,13 @@
+function increament() {
+	var number= Number(document.getElementById('number').innerHTML)
+	document.getElementById('number').innerHTML = number+1
+}
+
+function decreament() {
+	var number= Number(document.getElementById('number').innerHTML)
+	document.getElementById('number').innerHTML = number-1
+}
+
+function neutral() {
+	document.getElementById('number').innerHTML = 0
+}
